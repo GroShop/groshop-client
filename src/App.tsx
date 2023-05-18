7cbfece8434f84ae45d8dc80c5404d1a83bdf26f
haribb <hari@brownbutton.io>
2023-05-18 17:04:06 +0530

    redux added

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,20 @@ import ForgotPsd from './screens/phone_num/forgot_password.screen';
 import OtpVerify from './screens/otp_verify/otp_verify.screen';
 import Home from './screens/home/home.screen';
 
+
 const App = () => {
   const Stack = createStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Splash" component={Splash} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="ForgotPsd" component={ForgotPsd} />
-        <Stack.Screen name="OtpVerify" component={OtpVerify} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Splash" component={Splash} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="ForgotPsd" component={ForgotPsd} />
+          <Stack.Screen name="OtpVerify" component={OtpVerify} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 };
 
